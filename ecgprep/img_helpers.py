64a2715b15ea5ee1a482e5319d2cbaf45edef6f4
antonio <antonior92@gmail.com>
2025-12-15 19:01:22 +0100

    Include vectorize function for all leads

--- ecgprep/img_helpers.py
+++ ecgprep/img_helpers.py
@@ -16,39 +16,58 @@ duration_short = 12.5
 duration_long = 50
 n_rows = 4
 n_cols = 4
-lead_positions = {
-    'DI': (0, 0),
-    'DII': (1, 0),
-    'DIII': (2, 0),
-    'AVR': (0, 1),
-    'AVL': (1, 1),
-    'AVF': (2, 1),
-    'V1': (0, 2),
-    'V2': (1, 2),
-    'V3': (2, 2),
-    'V4': (0, 3),
-    'V5': (1, 3),
-    'V6': (2, 3),
-    'long DII': (3, 0)
-}
-y_offset = - (n_grid_h / n_rows) * grid_to_mm
-x_offset = (n_grid_w / n_cols) * grid_to_mm
 
 
-def estimate_pixels_per_mm(img):
-    height, width = img.shape
-    pixels_per_mm_h = height / (n_grid_h * grid_to_mm)
-    pixels_per_mm_w = width / (n_grid_w * grid_to_mm)
-    return pixels_per_mm_h, pixels_per_mm_w
+def bounding_boxes(height, width, n_rows = 4, n_cols = 4, lead_positions=None, h_offset=0, w_offset=0):
+    if lead_positions is None:
+        lead_positions = {
+            'DI': (0, 0),
+            'DII': (1, 0),
+            'DIII': (2, 0),
+            'AVR': (0, 1),
+            'AVL': (1, 1),
+            'AVF': (2, 1),
+            'V1': (0, 2),
+            'V2': (1, 2),
+            'V3': (2, 2),
+            'V4': (0, 3),
+            'V5': (1, 3),
+            'V6': (2, 3),
+            'long DII': (3, 0)
+        }
+    tlbr_boxes = {}
+
+    for lead_name in lead_positions.keys():
+        # Save leads using cv2
+        positions = lead_positions[lead_name]
+        block_h = height // n_rows
+        block_w = width // n_cols
+        h_start = positions[0] * block_h + h_offset
+        h_end = height - 1 + h_offset if 'long' in lead_name else h_start + block_h
+        w_start = positions[1] * block_w + w_offset
+        w_end = width - 1 + w_offset if 'long' in lead_name else w_start + block_w
+
+        tlbr_boxes[lead_name] = [h_start, w_start, h_end, w_end]
+
+    return tlbr_boxes
+
+
+class ScaleFromPixels():
+    def __init__(self, height, width):
+        self.pixels_per_mm_h = height / (n_grid_h * grid_to_mm)
+        self.pixels_per_mm_w = width / (n_grid_w * grid_to_mm)
+
+    def __call__(self, signal):
+        y = signal * mm_to_mv / self.pixels_per_mm_h
+        sample_frequency = self.pixels_per_mm_w / (mm_to_s)
+        return y, sample_frequency
 
 def signal_to_ecg(signal, pixels_per_mm_h, pixels_per_mm_w):
-    y = (signal - y_offset) * mm_to_mv / pixels_per_mm_h
+    y = signal * mm_to_mv / pixels_per_mm_h
     sample_frequency = pixels_per_mm_w / (mm_to_s)
     return y, sample_frequency
 
-def vectorize(lead_img, mask = None):
-    if mask is None:
-        mask = lead_img < 200
+def vectorize_single_lead(lead_img, mask):
     H, W = mask.shape[0], mask.shape[1]
     signal_pixels = np.zeros(W)
     for i in range(W):
@@ -61,3 +80,21 @@ def vectorize(lead_img, mask = None):
     # might be good to do something smarter to potentially remove some artifacts
 
     return signal_pixels
+
+
+def vectorize(img, mask, tlbr_boxes, scale_from_pixels):
+    # Find big box
+    BB = np.array(list(tlbr_boxes.values()))
+    start_w_pixel = min(BB[:, 1])
+    end_w_pixel = max(BB[:, 3])
+
+    # Define signal matrix
+    X = np.zeros((len(tlbr_boxes),  end_w_pixel  - start_w_pixel))
+    for i, (t, l, b, r) in enumerate(tlbr_boxes.values()):
+        X[i, l-start_w_pixel:r-start_w_pixel] = vectorize_single_lead(img[t:b, l:r],  mask[t:b, l:r] )
+        i+=1
+
+    ecg, sample_rate = scale_from_pixels(X)
+    leads = tlbr_boxes.keys()
+
+    return ecg, sample_rate, leads
--- test/test_img_helpers.py
+++ test/test_img_helpers.py
@@ -1,13 +1,18 @@
-from ecgprep.img_helpers import vectorize, signal_to_ecg
+from ecgprep.img_helpers import vectorize_single_lead, signal_to_ecg, bounding_boxes, ScaleFromPixels, vectorize
 from ecgprep.read_ecg import read_ecg
 from ecgprep.preprocess import preprocess_ecg
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image, ImageDraw
 
 
+PLOT = True
 
-# Test vectorize
-def test_vectorize():
+
+# Test vectorize_single_lead
+def test_vectorize_single_lead():
     ecg_from_signal, freq_signal, lead_names = read_ecg('ptbxl/00001_hr', format='wfdb')
 
     lead_src_img = cv2.imread('img/ptbxl-individual-leads/long DII.png')
@@ -15,7 +20,7 @@ def test_vectorize():
 
     lead_img = cv2.cvtColor(lead_src_img, cv2.COLOR_RGB2GRAY)
     mask_img = cv2.cvtColor(mask_src_img, cv2.COLOR_RGB2GRAY)>240
-    signal_from_image = vectorize(lead_img, mask_img)
+    signal_from_image = vectorize_single_lead(lead_img, mask_img)
 
     assert signal_from_image .shape[0] == lead_img.shape[1]
 
@@ -40,10 +45,61 @@ def test_vectorize():
     error = np.mean(np.abs(ecg_1 - ecg_2))
     assert error < 0.05
 
-    # see visually
-    import matplotlib.pyplot as plt
+    if PLOT:
+        plt.plot(ecg_1)
+        plt.plot(ecg_2)
+        plt.show()
 
-    plt.plot(ecg_1)
-    plt.plot(ecg_2)
-    plt.show()
+
+def test_bounding_boxes():
+    # Read image
+    img = cv2.imread("img/ptbxl.png")
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    border = 10
+    height, width = img.shape
+    tlbr_boxes = bounding_boxes(height - 2 * border, width - 2 * border, h_offset=border, w_offset=border)
+
+    if PLOT:
+        # Overlay boxes (assuming tlbr_boxes are (top, left, bottom, right))
+        pil_img = Image.fromarray(img)
+        overlay = pil_img.convert("RGB")
+        draw = ImageDraw.Draw(overlay)
+        for name, (t, l, b, r) in zip(tlbr_boxes.keys(), tlbr_boxes.values()):
+            draw.rectangle([l, t, r, b], outline=(255, 0, 0), width=2)
+            draw.text((l+5, t+ 5), name, fill="red")
+        overlay.show()
+
+def test_vectorize():
+    # Read image
+    img = cv2.imread("img/ptbxl.png")
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Get basic dimensions
+    border = 10
+    height, width = img.shape
+
+    # Get mask
+    img_clean = cv2.imread("img/ptbxl-clean.png")
+    img_clean  = cv2.cvtColor(img_clean, cv2.COLOR_RGB2GRAY)
+    mask = img_clean < 240
+
+
+    # Compute bounding boxes
+    tlbr_boxes = bounding_boxes(height - 2 * border, width - 2 * border, h_offset=border, w_offset=border)
+
+    # Get scaler
+    scale_from_pixels = ScaleFromPixels(height - 2 * border, width - 2 * border)
+
+    # Put all together
+    ecg, sample_rate, leads = vectorize(img, mask, tlbr_boxes, scale_from_pixels)
+
+    print(ecg, sample_rate, leads)
+
+
+
+if  __name__ == '__main__':
+    pass
 
